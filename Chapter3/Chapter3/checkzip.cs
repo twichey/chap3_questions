ff27ac37923d1ac74e156e0b0b6e5c3245f231dd
twichey <bhawley321@gmail.com>
2013-10-25 08:24:15 -0400

    fineshed zip

---
+++ Chapter3/Chapter3/checkzip.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter3
+{
+    class checkzip
+    {
+        static void main(string[] args)
+        {
+            string input;
+            int counter = 0;
+
+            int[] zipcodes = new int[10] { 18041, 18042, 18043, 18044, 18045, 18046, 18047, 18048, 18049, 18050 };
+
+            for (int i = 0; i < zipcodes.Length; i++)
+            {
+                Console.WriteLine(zipcodes[i]);
+            }
+
+            while (counter < zipcodes.Length)
+            {
+                Console.WriteLine(zipcodes[counter]);
+                counter++;
+            }
+
+            Console.WriteLine("type your zip code");
+
+            input = Console.ReadLine();
+
+            for (int i = 0; i < 100; i++) ;
+            {
+
+                if (input == zipcodes[i])
+                {
+                    Console.WriteLine("we can dieliver to your area");
+                }
+                else
+                {
+                    Console.WriteLine("we can not delever to your area");
+                }
+            }
+        }
+    }
+}
--- Chapter3/Furniture/Program.cs
+++ Chapter3/Furniture/Program.cs
@@ -9,6 +9,30 @@ namespace Furniture
     {
         static void Main(string[] args)
         {
+            string input;
+
+            Console.WriteLine("type P for Pine O for Oak M for Mahogany");
+            input = Console.ReadLine();
+
+            if (input == "P")
+            {
+                Console.WriteLine("price is $100");
+            }
+
+            if (input == "O")
+            {
+                Console.WriteLine("price is $225");
+            }
+
+            if (input == "M")
+            {
+                Console.WriteLine("price is $310");
+            }
+
+            if (input != "P" && input != "O" && input != "M")
+            {
+                Console.WriteLine("price is $0");
+            }
         }
     }
 }
